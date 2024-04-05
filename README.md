f0cfa4833be77532c3adab67814d0adc36cd1993
Victor E González P <vemiliogp@gmail.com>
2024-04-05 23:03:46 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+```
+Options:
+      --help         Show help                                         [boolean]
+      --version      Show version number                               [boolean]
+  -b, --base         Multiplication table base               [number] [required]
+  -l, --limit        Multiplication table limit           [number] [default: 10]
+  -s, --show         Show multiplication table        [boolean] [default: false]
+  -n, --name         File name        [string] [default: "multiplication-table"]
+  -d, --destination  File destination              [string] [default: "outputs"]
+```
---
+++ src/domain/use-cases/save-file.use-case.ts
@@ -0,0 +1,26 @@
+import fs from "fs";
+
+export interface SaveFileUseCase {
+  execute: (options: Options) => boolean;
+}
+
+export interface Options {
+  fileContent: string;
+  fileDestination: string;
+  fileName: string;
+}
+
+export class SaveFile implements SaveFileUseCase {
+  constructor() {}
+
+  execute({ fileContent, fileDestination, fileName }: Options): boolean {
+    try {
+      fs.mkdirSync(fileDestination, { recursive: true });
+      fs.writeFileSync(`${fileDestination}/${fileName}.txt`, fileContent);
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
+}
